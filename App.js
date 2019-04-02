f17bfe0eace82a26a70385cbd1e4dda5f34045df
Ammar <ammar3019@gmail.com>
2019-04-02 18:12:22 +0500

    Added Splash Screen

--- App.js
+++ App.js
@@ -8,12 +8,40 @@
 
 import React, {Component} from 'react';
 import HomeScreen from './src/Screens/HomeScreen'
-
+import SplashScreen from './src/Screens/SplashScreen'
 
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true }
+  }
+
+  performTimeConsumingTask = async() => {
+    return new Promise((resolve) =>
+        setTimeout(
+            () => { resolve('result') },
+            2000
+        )
+    );
+  }
+
+  async componentDidMount() {
+    // Preload data from an external API
+    // Preload data using AsyncStorage
+    const data = await this.performTimeConsumingTask();
+
+    if (data !== null) {
+      this.setState({ isLoading: false });
+    }
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return <SplashScreen />;
+    }
     return (
         <HomeScreen/>
     )
