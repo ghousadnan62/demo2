4c1fdd390f292fd1b51af89169dfd067546b008b
ghous <ghousadnan62@gmail.com>
2019-04-02 05:41:15 -0700

    React Navigation is working; Header Removed

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import ScreenManager from './src/Screens/ScreenManager'
-
 
 
 type Props = {};
