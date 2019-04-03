0d055f6730f5989bd3651e314124c70e28bf5649
ghous <ghousadnan62@gmail.com>
2019-04-03 03:53:38 -0700

    Previous Working Stage

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import HomeScreen from './src/Screens/HomeScreen'
 import SplashScreen from './src/Screens/SplashScreen'
@@ -14,40 +6,36 @@ import SplashScreen from './src/Screens/SplashScreen'
 type Props = {};
 export default class App extends Component<Props> {
 
-  constructor(props) {
-    super(props);
-    this.state = { isLoading: true }
-  }
+    constructor(props) {
+        super(props);
+        this.state = { isLoading: true }
+    }
 
-  performTimeConsumingTask = async() => {
-    return new Promise((resolve) =>
-        setTimeout(
-            () => { resolve('result') },
-            2000
+    performTimeConsumingTask = async() => {
+        return new Promise((resolve) =>
+            setTimeout(
+                () => { resolve('result') },
+                2000
+            )
+        );
+    };
+
+    async componentDidMount() {
+        // Preload data from an external API
+        // Preload data using AsyncStorage
+        const data = await this.performTimeConsumingTask();
+
+        if (data !== null) {
+            this.setState({ isLoading: false });
+        }
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            return <SplashScreen />;
+        }
+        return (
+            <HomeScreen/>
         )
-    );
-  }
-
-  async componentDidMount() {
-    // Preload data from an external API
-    // Preload data using AsyncStorage
-    const data = await this.performTimeConsumingTask();
-
-    if (data !== null) {
-      this.setState({ isLoading: false });
     }
-  }
-
-  render() {
-    if (this.state.isLoading) {
-      return <SplashScreen />;
-    }
-    return (
-        <HomeScreen/>
-    )
-  }
 }
-
-
-
-
