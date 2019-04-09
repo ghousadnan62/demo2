54a1b6a2816d929ddb473de897ac1d94e059fb6a
ghous <ghousadnan62@gmail.com>
2019-04-09 08:39:05 -0700

    Slider Added in Designer Tab

--- src/components/Tabs/DesignerWomenTab.js
+++ src/components/Tabs/DesignerWomenTab.js
@@ -1,79 +1,94 @@
 import React from 'react';
 import {View, Text} from "native-base";
-import MiniCardComp from '../Cards/MiniCardComp'
+import MiniCardComp from '../Cards/MiniCardComp';
+import SwiperCardComp from './SwiperCardsComp';
+
 
 const DesignerWomenTab = () => {
     return (
-        <View style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-        }}>
+
+        <View>
+            <View style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <Text style={{fontWeight:'bold',fontSize: 20}}> Popular Designers</Text>
+
+            </View>
+            <SwiperCardComp/>
+
+            <View style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
 
 
-            <Text
-                style={{
-                    fontSize: 20,
-                    fontWeight: 'bold'
-                }}
-            > Popular Desingers</Text>
+                <Text
+                    style={{
+                        fontSize: 20,
+                        fontWeight: 'bold'
+                    }}
+                > Popular Desingers</Text>
 
-            <View
-                style={{
-                    display:'flex',
-                    flexDirection: 'row',
-                    flexWrap: 'wrap',
-                    justifyContent: 'space-around'
-                }}
-            >
-                <MiniCardComp
-                    imgSource={'https://images.prod.meredith.com/product/ddd17df35c0d0e251e199b2fc8585602/1519882213810/l/no-prob-llama-shirt-no-probllama-shirt-graphic-tees-for-women-t-shirts-for-women-shirts-with-sayings-mom-shirt-mom-life-shirt'}
-                    catagoryName={'Guci'}
-                    cardHeight={150}
-                    cardWidth={110}
-                    Margin={0}
-                />
+                <View
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        flexWrap: 'wrap',
+                        justifyContent: 'space-around'
+                    }}
+                >
+                    <MiniCardComp
+                        imgSource={'https://images.prod.meredith.com/product/ddd17df35c0d0e251e199b2fc8585602/1519882213810/l/no-prob-llama-shirt-no-probllama-shirt-graphic-tees-for-women-t-shirts-for-women-shirts-with-sayings-mom-shirt-mom-life-shirt'}
+                        catagoryName={'Guci'}
+                        cardHeight={150}
+                        cardWidth={110}
+                        Margin={0}
+                    />
 
-                <MiniCardComp
-                    imgSource={'https://images.prod.meredith.com/product/ddd17df35c0d0e251e199b2fc8585602/1519882213810/l/no-prob-llama-shirt-no-probllama-shirt-graphic-tees-for-women-t-shirts-for-women-shirts-with-sayings-mom-shirt-mom-life-shirt'}
-                    catagoryName={'Calvin Klein'}
-                    cardHeight={150}
-                    cardWidth={110}
-                    Margin={0}
-                />
+                    <MiniCardComp
+                        imgSource={'https://images.prod.meredith.com/product/ddd17df35c0d0e251e199b2fc8585602/1519882213810/l/no-prob-llama-shirt-no-probllama-shirt-graphic-tees-for-women-t-shirts-for-women-shirts-with-sayings-mom-shirt-mom-life-shirt'}
+                        catagoryName={'Calvin Klein'}
+                        cardHeight={150}
+                        cardWidth={110}
+                        Margin={0}
+                    />
 
-                <MiniCardComp
-                    imgSource={'https://images.prod.meredith.com/product/ddd17df35c0d0e251e199b2fc8585602/1519882213810/l/no-prob-llama-shirt-no-probllama-shirt-graphic-tees-for-women-t-shirts-for-women-shirts-with-sayings-mom-shirt-mom-life-shirt'}
-                    catagoryName={'Lacoste'}
-                    cardHeight={150}
-                    cardWidth={110}
-                    Margin={0}
-                />
+                    <MiniCardComp
+                        imgSource={'https://images.prod.meredith.com/product/ddd17df35c0d0e251e199b2fc8585602/1519882213810/l/no-prob-llama-shirt-no-probllama-shirt-graphic-tees-for-women-t-shirts-for-women-shirts-with-sayings-mom-shirt-mom-life-shirt'}
+                        catagoryName={'Lacoste'}
+                        cardHeight={150}
+                        cardWidth={110}
+                        Margin={0}
+                    />
 
-                <MiniCardComp
-                    imgSource={'https://images.prod.meredith.com/product/ddd17df35c0d0e251e199b2fc8585602/1519882213810/l/no-prob-llama-shirt-no-probllama-shirt-graphic-tees-for-women-t-shirts-for-women-shirts-with-sayings-mom-shirt-mom-life-shirt'}
-                    catagoryName={'Guci'}
-                    cardHeight={150}
-                    cardWidth={110}
-                    Margin={0}
-                />
+                    <MiniCardComp
+                        imgSource={'https://images.prod.meredith.com/product/ddd17df35c0d0e251e199b2fc8585602/1519882213810/l/no-prob-llama-shirt-no-probllama-shirt-graphic-tees-for-women-t-shirts-for-women-shirts-with-sayings-mom-shirt-mom-life-shirt'}
+                        catagoryName={'Guci'}
+                        cardHeight={150}
+                        cardWidth={110}
+                        Margin={0}
+                    />
 
-                <MiniCardComp
-                    imgSource={'https://images.prod.meredith.com/product/ddd17df35c0d0e251e199b2fc8585602/1519882213810/l/no-prob-llama-shirt-no-probllama-shirt-graphic-tees-for-women-t-shirts-for-women-shirts-with-sayings-mom-shirt-mom-life-shirt'}
-                    catagoryName={'Calvin Klein'}
-                    cardHeight={150}
-                    cardWidth={110}
-                    Margin={0}
-                />
+                    <MiniCardComp
+                        imgSource={'https://images.prod.meredith.com/product/ddd17df35c0d0e251e199b2fc8585602/1519882213810/l/no-prob-llama-shirt-no-probllama-shirt-graphic-tees-for-women-t-shirts-for-women-shirts-with-sayings-mom-shirt-mom-life-shirt'}
+                        catagoryName={'Calvin Klein'}
+                        cardHeight={150}
+                        cardWidth={110}
+                        Margin={0}
+                    />
 
-                <MiniCardComp
-                    imgSource={'https://images.prod.meredith.com/product/ddd17df35c0d0e251e199b2fc8585602/1519882213810/l/no-prob-llama-shirt-no-probllama-shirt-graphic-tees-for-women-t-shirts-for-women-shirts-with-sayings-mom-shirt-mom-life-shirt'}
-                    catagoryName={'Lacoste'}
-                    cardHeight={150}
-                    cardWidth={110}
-                    Margin={0}
-                />
+                    <MiniCardComp
+                        imgSource={'https://images.prod.meredith.com/product/ddd17df35c0d0e251e199b2fc8585602/1519882213810/l/no-prob-llama-shirt-no-probllama-shirt-graphic-tees-for-women-t-shirts-for-women-shirts-with-sayings-mom-shirt-mom-life-shirt'}
+                        catagoryName={'Lacoste'}
+                        cardHeight={150}
+                        cardWidth={110}
+                        Margin={0}
+                    />
 
+                </View>
             </View>
         </View>
     );
